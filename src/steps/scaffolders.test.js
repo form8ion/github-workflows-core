ed50912def1bfa31f4b9df55d153823630655718
Matt Travi <programmer@travi.org>
2024-08-24 23:28:25 -0500

    test(unit): switch to vitest from mocha

---
+++ src/steps/scaffolders.test.js
@@ -0,0 +1,34 @@
+import {describe, expect, it} from 'vitest';
+
+import {checkout, executeVerification, installDependencies, setupNode} from './scaffolders.js';
+
+describe('step scaffolders', () => {
+  it('should scaffold the details of the checkout step', () => {
+    expect(checkout()).toEqual({uses: 'actions/checkout@v4.1.7'});
+  });
+
+  it('should set up node correctly when the version is determined from the `.nvmrc`', () => {
+    expect(setupNode({versionDeterminedBy: 'nvmrc'})).toEqual({
+      name: 'Setup node',
+      uses: 'actions/setup-node@v4.0.3',
+      with: {'node-version-file': '.nvmrc', cache: 'npm'}
+    });
+  });
+
+  it('should set up node correctly when the version is determined based on a matrix', () => {
+    expect(setupNode({versionDeterminedBy: 'matrix'})).toEqual({
+      name: 'Setup node',
+      uses: 'actions/setup-node@v4.0.3',
+      // eslint-disable-next-line no-template-curly-in-string
+      with: {'node-version': '${{ matrix.node }}', cache: 'npm'}
+    });
+  });
+
+  it('should install dependencies correctly', () => {
+    expect(installDependencies()).toEqual([{run: 'npm clean-install'}, {run: 'corepack npm audit signatures'}]);
+  });
+
+  it('should execute verification correctly', () => {
+    expect(executeVerification()).toEqual({run: 'npm test'});
+  });
+});
